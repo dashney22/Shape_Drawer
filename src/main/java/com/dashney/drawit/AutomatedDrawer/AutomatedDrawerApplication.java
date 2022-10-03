e6cddf30fb543bf14bae38cb47a5c7695dd0f77a
dashney22 <dashneymakgafela@gmail.com>
2022-10-03 14:02:56 +0200

    added home page

--- src/main/java/com/dashney/drawit/AutomatedDrawer/AutomatedDrawerApplication.java
+++ src/main/java/com/dashney/drawit/AutomatedDrawer/AutomatedDrawerApplication.java
@@ -2,8 +2,9 @@ package com.dashney.drawit.AutomatedDrawer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class AutomatedDrawerApplication {
 
 	public static void main(String[] args) {
