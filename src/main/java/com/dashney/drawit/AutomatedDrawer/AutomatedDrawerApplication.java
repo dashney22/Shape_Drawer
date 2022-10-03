ac71980a0caaba51953214946584d6b5ded7ac7b
dashney22 <dashneymakgafela@gmail.com>
2022-10-03 10:13:05 +0200

    Pushing Initial Project to Remote

---
+++ src/main/java/com/dashney/drawit/AutomatedDrawer/AutomatedDrawerApplication.java
@@ -0,0 +1,13 @@
+package com.dashney.drawit.AutomatedDrawer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatedDrawerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomatedDrawerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dashney/drawit/AutomatedDrawer/AutomatedDrawerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dashney.drawit.AutomatedDrawer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomatedDrawerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
